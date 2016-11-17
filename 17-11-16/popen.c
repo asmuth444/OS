a25d90d6555483f49a16a21535ca8e666cf5600b
Abhishek Roul <asmuth444@gmail.com>
2016-11-17 16:16:57 +0530

    17-11-16 Update

--- 17-11-16/popen.c
+++ 17-11-16/popen.c
@@ -1,12 +1,13 @@
 #include<stdlib.h>
 #include<unistd.h>
 #include<stdio.h>
+#include<string.h>
 int main()
 {
     FILE *f;
     char buff[50];
     sprintf(buff,"Hello World");
     f = popen("wc -c","w");
-    fwrite(buff,1,50,f);
+    fwrite(buff,1,strlen(buff),f);
     fclose(f);
 }
